0de37c9e767e3aa8abe6432fa9c0e538ef9f1e72
omakuandy@gmail.com <omakuandy@gmail.com>
2026-08-23 15:57:41 +0000

    Changed user's email so that the ai agent can use the current user data to perform requested actions using the MCP server

--- setup_secrets.py
+++ setup_secrets.py
@@ -12,14 +12,14 @@ import getpass
 
 w = WorkspaceClient()
 
-w.secrets.create_scope(scope="massive")
-w.secrets.put_secret(
-    scope="massive",
-    key="api-key",
-    string_value=getpass.getpass("Paste your Massive API key: ")
-)
+# w.secrets.create_scope(scope="massive")
+# w.secrets.put_secret(
+#     scope="massive",
+#     key="api-key",
+#     string_value=getpass.getpass("Paste your Massive API key: ")
+# )
 
-w.secrets.create_scope(scope="database")
+# w.secrets.create_scope(scope="database")
 w.secrets.put_secret(
     scope="database",
     key="alpaca-key-id",
@@ -44,8 +44,8 @@ w.secrets.put_acl(
     permission=workspace.AclPermission.READ,
 )
 
-w.secrets.put_acl(
-    scope="massive",
-    principal="users",
-    permission=workspace.AclPermission.READ,
-)
+# w.secrets.put_acl(
+#     scope="massive",
+#     principal="users",
+#     permission=workspace.AclPermission.READ,
+# )
